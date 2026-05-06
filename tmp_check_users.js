eb36f0fa0fd336549ee68175d8ec88aa2863421a
estudiodigitalluan <estudio.digitalluan@gmail.com>
2026-05-06 20:25:15 -0300

    removendo prisma

--- tmp_check_users.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.user.findMany()
-  .then(users => console.log(JSON.stringify(users, null, 2)))
-  .catch(e => console.error(e))
-  .finally(() => prisma.$disconnect());
